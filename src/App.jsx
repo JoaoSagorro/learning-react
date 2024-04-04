7c452697771bb672f7957c972d20a6d6e96bb7c5
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-04 13:48:33 +0100

    perfected the useState

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { EXAMPLES } from './data.js';
 
 function App() {
 
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleSelect(buttonSelected) {
     // buttonSelected => string with the button name
@@ -41,15 +41,17 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+            {!selectedTopic ? <p>Please select a topic</p> :
+              (<div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>)
+            }
         </section>
         <h2>Time to get started!</h2>
       </main>
