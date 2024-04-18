745dd74723a4f31c817094ba75922ce3e5f95ed3
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-18 08:15:41 +0100

    changing the name of the player

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+
+  const [ playerName, setPlayerName ] = useState(initialName)
 
   const [ isEditing, setIsEditing ] = useState(false);
 
 
-  function handleClick() {
-    setIsEditing(!isEditing)
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
-  const playerInput = isEditing ? <input></input> : (<span className="player-name">
-  {name}
-</span>)
+
+  function handleClick() {
+    // setIsEditing(!isEditing) this should not be use. Instead we should do the following as best practice:
+    setIsEditing((editing) => !editing)
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+
+  if (isEditing) {
+    editablePlayerName = (<input type="text" required value={playerName} onChange={handleChange}/>);
+  }
 
   return(
     <li>
       <span className="player">
-        {playerInput}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleClick}>
